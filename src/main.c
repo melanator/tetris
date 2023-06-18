e5ecad085f138dd89862c2768482714b3b7a4c47
melanator <ashevchuk22@gmail.com>
2023-06-18 21:00:10 +1100

    Commit

--- src/main.c
+++ src/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define TETRIS_WIDTH 12*2                                   /*  Tiles 2 pixels long     */
-#define TETRIS_HEIGHT 20
+#define TETRIS_WIDTH    24                                /*  Tiles 2 pixels long, tetris width 12     */
+#define TETRIS_HEIGHT   20
+#define TOTAL_WIDTH     80
+#define TILE            'X'
+
 #define clear() printf("\033[H\033[J")                      /*  Clear all terminal board */
-#define gotoxy(x,y) printf("\033[%d;%dH", (y), (x))         /*  Puts cursor to X, Y      */
+#define gotoxy(x, y) printf("\033[%d;%dH", (y), (x))         /*  Puts cursor to X, Y      */
 
 typedef struct shape {
     int width, height;
@@ -35,7 +38,7 @@ char board[TETRIS_HEIGHT][TETRIS_WIDTH] = {
 };
 
 void print_board(){
-    printf("╔════════════════════════╗");
+    printf("╔════════════════════════╗                                                            ");
     printf("\n");
     for (int i = 0; i < TETRIS_HEIGHT; i++){
         printf("║");
@@ -47,10 +50,43 @@ void print_board(){
     }
     printf("╚════════════════════════╝");
     printf("\n");
+    fflush(NULL);
+}
+
+void print_next_tile(){
+    const unsigned tile_size = 4;
+    gotoxy(30, 1);
+    printf("NEXT TILE:");
+    gotoxy(30, 2);
+    printf("╔════════╗");
+    for (int i = 0; i < 4; i++){
+        gotoxy(30, i+3);
+        printf("║        ║");
+    }
+    gotoxy(30, tile_size+3);
+    printf("╚════════╝");
+}
+
+void update_tile(unsigned x, unsigned y){
+    board[y][2*x] = TILE;
+    board[y][2*x+1] = TILE;
+}
+
+void refresh_screen(){
+    print_board();
+    print_next_tile();
+    gotoxy(30, 10);
+    printf("Score %d", 0);
+    gotoxy(0, TETRIS_HEIGHT+3);   // + 3 because of frames
 }
 
 int main(int argc, char **argv){
-    print_board();       
+    refresh_screen();
+    update_tile(1,1);
+    update_tile(2,2);
+    sleep(1);
+    clear();
+    refresh_screen();
     return 0;
 }
 
